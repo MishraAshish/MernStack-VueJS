f779f5a21ba81667c5f23a1ec7926bf3f40c0c46
Ashish <ash.ishu025g@gmail.com>
2020-06-10 22:56:14 +0530

    operators in js

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -1,36 +1,25 @@
-// UnTyped 
+var mybool = true;
 
-//var unTypedVariable = "Cyprian";//string
-//var unTypedVariable = 0.53622;//decimal
-//var unTypedVariable = new Date();//date time
-//var unTypedVariable = 998989989;//integer
-//console.log(unTypedVariable); 
+// or operator ( || )
+// if (mybool || mybool === 1) {
+//     console.log("Or Operator True (if)");
+// } else {
+//     console.log("Or Operator False (else)");
+// }
 
-//Dynamic Typing : undefined, string, number, null, object
 
-console.log(dynamicTypedVariable); //undefined : its a valid data type in js
-console.log(typeof dynamicTypedVariable);
+// and operator ( && )
+if (mybool && mybool == 1) { // double equal to operator : where it simply compares value not type
+    console.log("And Operator True (if)");
+} else {
+    console.log("And Operator False (else)");
+}
 
-var dynamicTypedVariable = "Cyprian";//string
-console.log(dynamicTypedVariable); 
-console.log(typeof dynamicTypedVariable);
+// triple equal-to operator : where it compares value as well as type
+if (mybool && mybool === 1) { 
+    console.log("And Operator True (if)");
+} else {
+    console.log("And Operator False (else)");
+}
 
-dynamicTypedVariable = 0.53622;//decimal : data type - number
-console.log(dynamicTypedVariable); 
-console.log(typeof dynamicTypedVariable);
-
-dynamicTypedVariable = new Date();//date time : data type - object
-console.log(dynamicTypedVariable); 
-console.log(typeof dynamicTypedVariable);
-
-dynamicTypedVariable = 998989989;//integer : data type - number
-console.log(dynamicTypedVariable); 
-console.log(typeof dynamicTypedVariable);
-
-dynamicTypedVariable = null;//null : data type - object
-console.log(dynamicTypedVariable); 
-console.log(typeof dynamicTypedVariable);
-
-dynamicTypedVariable = {};//object : data type - object  //new Object()
-console.log(dynamicTypedVariable); 
-console.log(typeof dynamicTypedVariable);
+// >, <, <=, >= 
