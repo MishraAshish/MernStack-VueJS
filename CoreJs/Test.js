f9ea1b2d39bae0ce67bf083208bdfd9b09bf6762
Ashish <ash.ishu025g@gmail.com>
2020-06-10 23:02:53 +0530

    commenting and un commenting

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -1,25 +1,24 @@
+//Commenting in javascript
+
+// ctrl+k+c - to comment
+// ctrl+k+u - to un-comment
+
+//- single line comment - 
+
+/*
+Multiline comment
+
 var mybool = true;
-
-// or operator ( || )
-// if (mybool || mybool === 1) {
-//     console.log("Or Operator True (if)");
-// } else {
-//     console.log("Or Operator False (else)");
-// }
-
-
-// and operator ( && )
-if (mybool && mybool == 1) { // double equal to operator : where it simply compares value not type
-    console.log("And Operator True (if)");
+or operator ( || )
+if (mybool || mybool === 1) {
+    console.log("Or Operator True (if)");
 } else {
-    console.log("And Operator False (else)");
+    console.log("Or Operator False (else)");
 }
+*/
 
-// triple equal-to operator : where it compares value as well as type
-if (mybool && mybool === 1) { 
-    console.log("And Operator True (if)");
-} else {
-    console.log("And Operator False (else)");
-}
 
-// >, <, <=, >= 
+//each line ends on semicolon ; - but its optional
+
+console.log("Test Me")
+console.log("Test Me");
