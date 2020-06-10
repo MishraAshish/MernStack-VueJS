a82d953abe9bc6a4cd1fd345ab11def56e074ac2
Ashish <ash.ishu025g@gmail.com>
2020-06-10 22:24:28 +0530

    hoisting case sensitive

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -2,22 +2,22 @@
 
 // Hoisting the process of creating a snap shot of all the variables and functions present in javascript file/module
 var myName = undefined;
-
+//var myname = "Cyprian";
 console.log(myName); //variable hoisting : variable is hoisted with default value "undefined" at the top of the function
 name("Brian"); // function hoisting : functions get hoisted at the top with its definition so we can exceute even before defining it
 
 myName = "Alleen"; //initialization of an identifier
 
+//var myname = "Cyprian";
+
 function name(params) { //defining a function 
-    console.log("Your name is :- ", params);
+    console.log("Your name is :- 1", params);
 
     //Hoisting for the inner variables and functions be limited to each function
-    var myName2 = "Cyprian";
-    function name2(myName2) { //defining a function 
-        console.log("Your name is :- ", myName2);
     
-        
-    }   
+    name2(myname);
+    function name2(myName2) { //defining a function 
+        console.log("Your name is :- 2", myName2);  
+    }
 }
-
 name(myName);
