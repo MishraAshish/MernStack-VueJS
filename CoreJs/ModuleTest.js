8e74ecf46d85fb6347859a8567810f7bc5375d1a
Ashish <ash.ishu025g@gmail.com>
2020-06-11 21:30:03 +0530

    create object various ways

--- CoreJs/ModuleTest.js
+++ CoreJs/ModuleTest.js
@@ -1,8 +1,29 @@
 // {} : JS Object as well as JSON object
 
+// This is the literals way of creating an object
 var User = {
     Name: "Brian",
-    Age : 18
+    Age : 18,
+    address : {
+        Line1 : "Line1",
+        Line2 : "Line2"
+    }
+}
+//module.exports = User;
+
+// Creating and Object with Object contructor
+var Student = new Object(User);
+//module.exports = Student;
+
+// Creating object with constructor function //Legacy way of creating object
+
+var ConstFuncObj = function (name, age) { // as functions are first class members of js
+    this.Name = name,
+    this.Age = age   
 }
 
-module.exports = User;
+var Employee = new ConstFuncObj("Alleen", 19); //Employee object from constructor function
+
+module.exports = Employee;
+
+module.exports = Employee;
