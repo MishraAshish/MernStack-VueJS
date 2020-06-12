7e218eb83b9476de51ecdfce5761d386279b5f53
Ashish <ash.ishu025g@gmail.com>
2020-06-12 20:48:37 +0530

    object.assign

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -1,10 +1,25 @@
-//Test : Module - is a javascript file in general
+//Object - in a little more detail
 
-var userObj = require("./ModuleTest"); // we are importing js file here so that we can you
+var User = {
+    Name: "Brian",
+    Age : 18,
+    Address : {
+        Line1 : "Line1",
+        Line2 : "Line2"
+    },
+    GetName : function () {
+        return this.Name;
+    }
+}
 
-console.log("User ", userObj);
-console.log("Get Name ", userObj.User.GetName())
+var User2 = {
+    Name: "Dennis",
+    Activity : "in lockdown",
+}
 
-console.log("Student Details : ", userObj.Student)
-console.log("Student Get Name : ", userObj.Student.GetName())
-console.log("Student Get Addess : ", userObj.Student.GetAddress())
+// User2 - Target , User - Source which copies all its values even common ones
+var User3 = Object.assign(User2, User);
+
+console.log("User 3", User3);
+console.log("User 2", User2);
+console.log("User ", User);
