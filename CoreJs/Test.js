e1c88bba1cb45f1592033340e89589d637da7fef
Ashish <ash.ishu025g@gmail.com>
2020-06-12 21:44:09 +0530

    function no overloading

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -7,14 +7,38 @@ function name(param1, param2, param3) {
     //body
     console.log("This is MERNStack")
 }
-name() //invocation
-
+//name() //invocation
+//console.log(sum(2,3,4));
 // Function expression 
-var sum = function (x, y) {
-    return x+y;
+// var sum = function (x, y) {
+//     console.log("Function Expression");
+//     return x+y;
+// }
+
+// console.log(sum(2,2))
+// console.log(sum(2)) //y = undefined , NaN : Not a Number
+// console.log(sum(2,3,4)) // 4:  ignored
+// console.log(sum()) //x,y = undefined , NaN : Not a Number
+
+// Function Overwriting no concept of overloading in javascript
+console.log(sum())
+
+function sum() {
+    console.log("Function No Param");
+    return 20;
 }
 
-console.log(sum(2,2))
-console.log(sum(2)) //y = undefined , NaN : Not a Number
-console.log(sum(2,3,4)) // 4:  ignored
-console.log(sum()) //x,y = undefined , NaN : Not a Number
+function sum(x,y,z) {
+    console.log("Function 1");
+    return x+y+z
+}
+
+console.log(sum(2,3,4));
+
+function sum(x) {
+    console.log("Function 2");
+    return x;
+}
+
+console.log(sum(2,3,4));
+
