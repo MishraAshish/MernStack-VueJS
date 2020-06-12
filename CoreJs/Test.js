fb533a61fab84b79322664faa0f973b444846fe5
Ashish <ash.ishu025g@gmail.com>
2020-06-12 20:51:38 +0530

    object.assign

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -18,8 +18,13 @@ var User2 = {
 }
 
 // User2 - Target , User - Source which copies all its values even common ones
-var User3 = Object.assign(User2, User);
+var User3 = Object.assign(User);
 
 console.log("User 3", User3);
 console.log("User 2", User2);
 console.log("User ", User);
+
+
+//If You need to stop prototype chain
+
+var NoProtoObj = Object.create(null);//this removes protoype chain of copied object which is null
