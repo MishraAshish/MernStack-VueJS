0022f8cd8d3ac531aa7e4600d4235674f1facdb9
Ashish <ash.ishu025g@gmail.com>
2020-06-15 22:04:22 +0530

    callback

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -1,18 +1,39 @@
-//Scope, Context and Call, Apply and Bind
+//Scope, Context and Call, Apply and => Bind
 
-var car = {
-    regNumber:"XXXXXXX",
-    brand:"Toyota"
+// bind :  Will keep holding the context and will not run immediately but supply the context when event is invoked
+
+
+// var user = {
+//     id: 42,
+//     printId: function() { 
+        
+//         setTimeout(function() {
+//             console.log("User Id is : ", this.id);
+//         }, 1000);
+
+//         //console.log("User Id is : ", this.id);
+//     }
+//    };
+
+// user.printId();
+
+
+
+//learning the call back function
+// a callback function executes in the executer function with same context it was supplied at run time
+
+var print = function (callbackFunction, param) { // a function could be recieved as a parameter is termed as call back function
+    console.log("Printing the callback : ", callbackFunction(param));
 }
 
-function displayCar(ownerName, ownerCity, ownerTele){
-
-    console.log(ownerName +"\n"+ ownerCity +"\n"+ ownerTele
-                +",\n this is your car : ",
-                this.regNumber + " \n  ",
-                this.brand
-                );    
+// below functions can be passed as callback functions
+function name() {
+    return "Alleen";
 }
 
-displayCar.call(car, "Jaya","NY",899545)
-displayCar.apply(car, ["Jaya","NY",899545])
+function age(age) {
+    return age;
+}
+
+print(name); //passing name as a callback function
+print(age, 22); //passing name as a callback function and a related parameter
