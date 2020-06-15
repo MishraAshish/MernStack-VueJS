4716410063b2d43b3ca4d76331aa0ec97187cef0
Ashish <ash.ishu025g@gmail.com>
2020-06-15 20:52:41 +0530

    call and apply example

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -1,41 +1,18 @@
 //Scope, Context and Call, Apply and Bind
 
-var person = {name: 'Marvin', age: 42, size: '2xM'};
-//name = "Cyprian"; //global variable
-var person1 = {name: 'Dennis', age: 21, size: '1xM'};
+var car = {
+    regNumber:"XXXXXXX",
+    brand:"Toyota"
+}
 
-var sayHello = function(bio)
-{
-    console.log('Hello, ' + this.name);
-    console.log('Age , ' + this.age);
+function displayCar(ownerName, ownerCity, ownerTele){
 
-    console.log('Bio , ' + bio);
-};
+    console.log(ownerName +"\n"+ ownerCity +"\n"+ ownerTele
+                +",\n this is your car : ",
+                this.regNumber + " \n  ",
+                this.brand
+                );    
+}
 
-var sayGoodbye = function(param1, param2, param3)
-{
-    console.log('Goodbye, ' + this.name);
-    console.log('Size , ' + this.size);
-};
-
-//sayHello();
-//sayGoodbye();
-
-sayHello.call(person, "MernStack Trainer"); //call allows us to change the context (execution context at runtime)
-//sayGoodbye.call(person);
-sayGoodbye.call(person1);
-
-//apply : is used to change the context at runtime like call but has the capablity to pass parameters in the form of array
-// is widely to create delegates
-
-var sayHelloForApply = function(addres1, address2, address3)
-{
-    console.log('Hello, ' + this.name);
-    console.log('Age , ' + this.age);
-
-    console.log('Address from Apply - ' + addres1);
-    console.log('Address from Apply - ' + address2);
-    console.log('Address from Apply - ' + address3);
-};
-
-sayHelloForApply.apply(person1, ["addres1 -sada", "address2 - dsada", "address3 -asdasda"]);
+displayCar.call(car, "Jaya","NY",899545)
+displayCar.apply(car, ["Jaya","NY",899545])
