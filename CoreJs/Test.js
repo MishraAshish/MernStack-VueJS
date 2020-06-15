ff5d9776e8e13f71c2b53c1f1527ec72310968e8
Ashish <ash.ishu025g@gmail.com>
2020-06-15 23:00:21 +0530

    bind functions

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -1,39 +1,19 @@
 //Scope, Context and Call, Apply and => Bind
 
 // bind :  Will keep holding the context and will not run immediately but supply the context when event is invoked
-
-
-// var user = {
-//     id: 42,
-//     printId: function() { 
+var user = {
+    id: 42,
+    printId: function() { 
         
-//         setTimeout(function() {
-//             console.log("User Id is : ", this.id);
-//         }, 1000);
+        setTimeout(
+            function() { // a function as a callback to settimeout
+                console.log("User Id is : ", this.id);
+            }.bind(this), //keeps attached the context for the time till your event executes
+                2000 //milli seconds
+            );
 
-//         //console.log("User Id is : ", this.id);
-//     }
-//    };
+        //console.log("User Id is : ", this.id);
+    }
+   };
 
-// user.printId();
-
-
-
-//learning the call back function
-// a callback function executes in the executer function with same context it was supplied at run time
-
-var print = function (callbackFunction, param) { // a function could be recieved as a parameter is termed as call back function
-    console.log("Printing the callback : ", callbackFunction(param));
-}
-
-// below functions can be passed as callback functions
-function name() {
-    return "Alleen";
-}
-
-function age(age) {
-    return age;
-}
-
-print(name); //passing name as a callback function
-print(age, 22); //passing name as a callback function and a related parameter
+user.printId();
