e78cec22fce978461ddf8236c9112fdf3e1a642c
Ashish <ash.ishu025g@gmail.com>
2020-06-18 22:12:12 +0530

    const is not immutable

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -1,27 +1,17 @@
 //lexical scope : { //lexical // } //let and const
 
-{
-  // console.log(functionScope)
-  // console.log(lexicalScope);
-  // console.log(lexicalScopeConst)
-  
-  var functionScope = "hoisting allowed";
+//const myConstValueType = "Alleen";
+///myConstValueType = "Dennis";
 
-  var functionScope = 36616;
 
-  let lexicalScope;
-  
-  lexicalScope = 98989898;
 
-  lexicalScope = "asdasdas";
+const myConstObjectType = {
+            name : "Alleen"
+          }
 
-  //let lexicalScope = "test test"; // values can be updated as many times but cant be redeclared
+    myConstObjectType.name = "Dennis";
 
-  const lexicalScopeConst = "sadsa"; //as soon as you define we need to declare and can't be redefined
-
-  // lexicalScopeConst = "asdasd";
-}
-
-// console.log(functionScope)
-// console.log(lexicalScope);
-// console.log(lexicalScopeConst)
+    // myConstObjectType = {
+    //   name : "Alleen"
+    // }
+    console.log(myConstObjectType);
