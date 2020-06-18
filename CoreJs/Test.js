0d0b361b0a5c5718c972eb7cb725723c4fb1bddb
Ashish <ash.ishu025g@gmail.com>
2020-06-18 22:33:02 +0530

    const is not immutable

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -1,17 +1,38 @@
-//lexical scope : { //lexical // } //let and const
+// ForIn and ForOF loop
 
-//const myConstValueType = "Alleen";
-///myConstValueType = "Dennis";
+//for in loop  
+let person = {
+  fname:"John", 
+  lname:"Doe", 
+  //lname:"asdas", 
+  age:25,
+  address : {line1: "", line2: ""}
+};  
+
+for (const key in person) {
+  //if (person.hasOwnProperty(key)) { //check validation
+    const element = person[key];
+    console.log("Current Item : ", element)
+  //}
+}
+
+var arr = [3, 5, 7, "MyVal"];
+arr.myVal = "Hello";
+
+for (let i of arr) {
+  console.log(i); 
+  // logs “3”, “5”, “7”
+  //it is does not log “3”, “5”, “7”,”hello”
+
+}
 
 
+//for of loop  
+ 
+let cars = ['BMW', 'Volvo', 'Mini'];
 
-const myConstObjectType = {
-            name : "Alleen"
-          }
+for (const iterator of cars) {
+    console.log("Current Item For Of:",iterator)
+}
 
-    myConstObjectType.name = "Dennis";
 
-    // myConstObjectType = {
-    //   name : "Alleen"
-    // }
-    console.log(myConstObjectType);
