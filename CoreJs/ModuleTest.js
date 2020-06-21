3efd9873e53cf85ff15358f74534c59b7b438b68
Ashish <ash.ishu025g@gmail.com>
2020-06-21 21:36:22 +0530

    bind

--- CoreJs/ModuleTest.js
+++ CoreJs/ModuleTest.js
@@ -61,4 +61,24 @@ console.log( Employee.GetName());
 console.log( Employee.GetAddress());
 
 
+//bind : will attach the context at run time and returns function unlike call and apply which returns funciton execution
 
+
+let userDetails = {
+    name : "Blaz",
+    age : 19,
+    
+    showDetails : function () {
+      //this : context of userDetails object
+      
+      setTimeout(function() { //call back function of settimeout
+          console.log({
+            name: this.name, // this : is global context because settimeout executes without any context being set
+            age: this.age
+          })
+      }.bind(this), // we are copying this which is userDetails as a context to settimeout callback
+      3000); //waiting time in milli seconds        
+    }
+}
+
+userDetails.showDetails();
--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -1,18 +1,24 @@
-//Functions :
-// Function CallBacks :
-var hisName = "hisname";
+// call and apply : setting context of given functions explicitely
+// both return fuction execution instantly
 
-function name(params) {
-  console.log(hisName);
-  return params;
+//bind : will attach the context at run time and returns function unlike call and apply which returns funciton execution
+
+
+let userDetails = {
+    name : "Blaz",
+    age : 19,
+    
+    showDetails : function () {
+      //this : context of userDetails object
+      
+      setTimeout(function() { //call back function of settimeout
+          console.log({
+            name: this.name, // this : is global context because settimeout executes without any context being set
+            age: this.age
+          })
+      }.bind(this), // we are copying this which is userDetails as a context to settimeout callback
+      3000); //waiting time in milli seconds        
+    }
 }
 
-
-function print(callBackFunc, param) { //Functions can be passed as parameter - callBack Func
-    console.log(callBackFunc(param))
-}
-
-
-print(name, "Blaz Demsar");
-
-print(name, "Cyprian");
+userDetails.showDetails();
