8fdf54067ed1e01e44ee46afa39b2993b169295f
Ashish <ash.ishu025g@gmail.com>
2020-06-22 20:58:03 +0530

    arrow function

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -1,24 +1,17 @@
-// call and apply : setting context of given functions explicitely
-// both return fuction execution instantly
+// Arrow Functions : Fat arrow functions 
+//1.  Short hand to write functions
 
-//bind : will attach the context at run time and returns function unlike call and apply which returns funciton execution
+let name = (param)=>console.log(param);
+name("God")
+
+let sum = (a,b) => a+b;
+console.log(sum(8,5))
 
 
-let userDetails = {
-    name : "Blaz",
-    age : 19,
-    
-    showDetails : function () {
-      //this : context of userDetails object
-      
-      setTimeout(function() { //call back function of settimeout
-          console.log({
-            name: this.name, // this : is global context because settimeout executes without any context being set
-            age: this.age
-          })
-      }.bind(this), // we are copying this which is userDetails as a context to settimeout callback
-      3000); //waiting time in milli seconds        
-    }
-}
+let big = (a,b,c) => {
+            let d= a+b;
+            return d*c
+          }
 
-userDetails.showDetails();
+console.log(big(8,5,10))
+//write an arrow function accepts three parameters it will add first add first two and then divide by third param
