b5818d4ca10ab2e3c4f670f68a74e305d799072e
Ashish <ash.ishu025g@gmail.com>
2020-06-22 22:40:47 +0530

    map and set

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -4,40 +4,30 @@ let myMap = new Map();
 
 let keyString = 'a string', keyObj = {}, keyFunc = function() {};
 
+let userObj = {
+  name:"Alleen",
+  age: 21
+} 
+
+let userObjDetails = {
+  name:"Alleen",
+  age: 21,
+  adress:{
+      add:"line1",
+      add:"line2"
+  },
+  bio: "asdas",
+  asda:"ASdas"
+} 
 // setting the values 
 myMap.set(keyString, "value associated with 'a string'"); 
 myMap.set(keyObj, 'value associated with keyObj'); 
 myMap.set(keyFunc, 'value associated with keyFunction'); 
 
-console.log(myMap.size);
+myMap.set(userObj, userObjDetails);
 
-console.log(myMap.get('a string'))
+//console.log(myMap.values());
 
-console.log(myMap.delete(keyObj))
-console.log(myMap.size);
+console.log(myMap.has(userObj))
 
-console.log(myMap.delete(keyObj))
-console.log(myMap.size);
-
-myMap.set("session","MERNSTack Session")
-
-console.log(myMap.values());
-
-//Sets 
-
-const set1  = new Set([1, 2, 3, 4, 5]);
-
-console.log(set1.has(1));
-// expected output: true
-
-console.log(set1.has(5));
-// expected output: true
-
-console.log(set1.has(6));
-// expected output: false
-
-
-const set2  = new Set(["Test", "Test2", 3, 4, 5]);
-
-console.log(set2.values());
-// expected output: true
+console.log(myMap.get(userObj))
