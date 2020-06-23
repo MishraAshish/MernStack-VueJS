8664202939141cdf2c3b03c458a699cb81190fc6
Ashish <ash.ishu025g@gmail.com>
2020-06-23 22:40:42 +0530

    arithmatic

--- CoreJs/Test.js
+++ CoreJs/Test.js
@@ -1,60 +1,25 @@
-// Iterators : filter, map, reduce and some
+/////////
+//generator functions
 
-let persons = [
-        {id : 1, name : "John",  tags : "javascript"},
-        {id : 2, name : "Alice", tags : "javascript"},
-        {id : 3, name : "Roger", tags : "java"},
-        {id : 4, name : "Adam",  tags : "javascript"},
-        {id : 5, name : "Alex",  tags : "java"}
-    ];
+//////////////////
+function *arithmetic(num1, num2){
+    
+    yield console.log("Addition : " + (num1+num2));
+    
+    yield console.log("Subtraction : " + (num1-num2));
+    
+    yield console.log("Multiply : " + (num1*num2));
+    
+    yield console.log("Division : " + (num1/num2));
+    
+    return "Done with Operations";
+}
 
-//Filter : give me the list of persons who's tag is javascript
+let calculator = arithmetic(6,2);
 
-let persJavasscript = persons.filter((person) => person.tags === "javascript")
+calculator.next();
+calculator.next();
+calculator.next();
+calculator.next();
 
-console.log(persJavasscript);
-
-//Map : give me the list of names who's tag is javascript
-
-let persNamesJavasscript = persons.map((person) => person.tags === "javascript" ? person.name : "").filter(obj => obj != "")
-
-console.log(persNamesJavasscript);
-
-
-//console.log(persons); // not manipulated in base object
-
-//Some : short circuit , when it finds value your're looking for it'll stop there and return ture/or false
-
-let hasPersonWithId10 = persons.some(person => person.id === 10)
-console.log(hasPersonWithId10);
-
-let isAdam = persons.some(person => person.name === "Adam")
-console.log(isAdam);
-
-
-//Reduce : iterates every object and takes the value returned from the first operation as current object
-
-let uniqueTags = persons.reduce((currentitem, personObj) => {
-                        //console.log(currentitem);
-                        //console.log(personObj);
-
-                        currentitem[personObj.tags] = 1;
-                        return currentitem;
-                    },{});
-//console.log(uniqueTags);
-
-
-let uniqueTagsCount = persons.reduce((currentitem, personObj) => {
-
-        console.log(currentitem);
-        
-        currentitem[personObj.tags] = currentitem[personObj.tags] ? currentitem[personObj.tags]+1 : 1;
-
-        return currentitem;
-    }, {}); //initialize the currentItem [], {}
-console.log(uniqueTagsCount);
-
-
-//list the person id's, having java as their tags
-//find if there are csharp persons in list
-//what is the tag of person with name adam
+console.log(calculator.next());
