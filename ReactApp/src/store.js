6a43ca353fa6509689cd8bbb2debc7973954711f
Ashish <ash.ishu025g@gmail.com>
2020-07-24 18:59:53 +0530

    currying and property shorthand in js

--- ReactApp/src/store.js
+++ ReactApp/src/store.js
@@ -5,14 +5,14 @@ import thunk from 'redux-thunk';//make aync call to api
 import user from "./App/State/UserReducer";
 
 let myLogger = () => (next) => (action) => {    //a custom middleware to log store interaction
-    console.log("Logged Action : Store File ", action);
+    console.log("Logged Action : Store File ", action); //currying in javasript where we pass function as input and recieve function as output
     next(action);
 };
 
 export default createStore(
     combineReducers( //club all the reducers that we have in our application like user, product ...
-    {
-        user : user
+    { //property shorthand in javascript
+        user : user 
     }),
     {},
     applyMiddleware(myLogger ,thunk ,promise)// adding middlewares that we are going to use in our applicatino
