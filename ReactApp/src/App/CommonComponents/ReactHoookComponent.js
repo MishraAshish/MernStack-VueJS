680cb08dce2127444f45f364901e55071cd3d598
Ashish <ash.ishu025g@gmail.com>
2020-08-25 21:52:21 +0530

    react hooks useEffect

--- ReactApp/src/App/CommonComponents/ReactHoookComponent.js
+++ ReactApp/src/App/CommonComponents/ReactHoookComponent.js
@@ -8,18 +8,21 @@ let ReactHook = (props)=>{
     let user = useSelector((state) => state.user.user); 
     let dispatchUser = useDispatch(); // using this hook to dispatch the addUserToStore action
 
+    let userUpdated = {
+        userName: "Alleen",
+        password: "xxxxxxx",
+        street: "NY-CA",
+        mobile: "0070070786"
+    };    
+
     let dispatchUserToReducer = ()=>{
+        alert("This user is going to dispatch : "+ JSON.stringify(userUpdated));
+        
+        //dispatchUser(addUserToStore(user))
 
-        let user = {
-            userName: "Alleen",
-            password: "xxxxxxx",
-            street: "NY-CA",
-            mobile: "0070070786"
-        }
-
-        alert("This user is going to dispatch : "+ JSON.stringify(user));
-
-        dispatchUser(addUserToStore(user));
+        useEffect(
+            dispatchUser(addUserToStore(userUpdated))
+        );        
     }
 
     return(
